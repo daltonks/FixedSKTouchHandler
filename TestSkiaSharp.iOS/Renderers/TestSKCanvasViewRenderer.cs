526c035459193d1d4d600e500653446ac510f646
Dalton Spillman <dalton@scribblebuddies.app>
2020-12-15 18:29:13 -0600

    Try ShouldRecognizeSimultaneously

--- TestSkiaSharp.iOS/Renderers/TestSKCanvasViewRenderer.cs
+++ TestSkiaSharp.iOS/Renderers/TestSKCanvasViewRenderer.cs
@@ -30,131 +30,112 @@ namespace TestSkiaSharp.iOS.Renderers
         }
     }
 
-	/// <summary>
-	/// https://github.com/mono/SkiaSharp/blob/master/source/SkiaSharp.Views.Forms/SkiaSharp.Views.Forms.iOS/SKTouchHandler.cs
-	/// </summary>
+    /// <summary>
+    /// https://github.com/mono/SkiaSharp/blob/master/source/SkiaSharp.Views.Forms/SkiaSharp.Views.Forms.iOS/SKTouchHandler.cs
+    /// </summary>
     internal class SKTouchHandler : UIGestureRecognizer
-	{
-        private readonly HashSet<UITouch> _touches = new HashSet<UITouch>();
+    {
+        private Action<SKTouchEventArgs> onTouchAction;
+        private Func<double, double, SKPoint> scalePixels;
 
-		private Action<SKTouchEventArgs> onTouchAction;
-		private Func<double, double, SKPoint> scalePixels;
+        public SKTouchHandler(Action<SKTouchEventArgs> onTouchAction, Func<double, double, SKPoint> scalePixels)
+        {
+            this.onTouchAction = onTouchAction;
+            this.scalePixels = scalePixels;
 
-		public SKTouchHandler(Action<SKTouchEventArgs> onTouchAction, Func<double, double, SKPoint> scalePixels)
-		{
-			this.onTouchAction = onTouchAction;
-			this.scalePixels = scalePixels;
+            DisablesUserInteraction = false;
+            ShouldRecognizeSimultaneously = (recognizer, gestureRecognizer) => false;
+        }
 
-			DisablesUserInteraction = false;
-		}
+        public bool DisablesUserInteraction { get; set; }
 
-		public bool DisablesUserInteraction { get; set; }
-
-		public void SetEnabled(UIView view, bool enableTouchEvents)
-		{
-			if (view != null)
-			{
-				if (!view.UserInteractionEnabled || DisablesUserInteraction)
-				{
-					view.UserInteractionEnabled = enableTouchEvents;
-				}
-				if (enableTouchEvents && view.GestureRecognizers?.Contains(this) != true)
-				{
-					view.AddGestureRecognizer(this);
-				}
-				else if (!enableTouchEvents && view.GestureRecognizers?.Contains(this) == true)
-				{
-					view.RemoveGestureRecognizer(this);
-				}
-			}
-		}
-
-		public void Detach(UIView view)
-		{
-			// clean the view
-			SetEnabled(view, false);
-
-			// remove references
-			onTouchAction = null;
-			scalePixels = null;
-		}
-
-		public override void TouchesBegan(NSSet touches, UIEvent evt)
-		{
-			base.TouchesBegan(touches, evt);
-
-			foreach (UITouch touch in touches.Cast<UITouch>())
-			{
-				if (FireEvent(SKTouchAction.Pressed, touch, true))
+        public void SetEnabled(UIView view, bool enableTouchEvents)
+        {
+            if (view != null)
+            {
+                if (!view.UserInteractionEnabled || DisablesUserInteraction)
                 {
-                    _touches.Add(touch);
+                    view.UserInteractionEnabled = enableTouchEvents;
                 }
-                else
+                if (enableTouchEvents && view.GestureRecognizers?.Contains(this) != true)
+                {
+                    view.AddGestureRecognizer(this);
+                }
+                else if (!enableTouchEvents && view.GestureRecognizers?.Contains(this) == true)
+                {
+                    view.RemoveGestureRecognizer(this);
+                }
+            }
+        }
+
+        public void Detach(UIView view)
+        {
+            // clean the view
+            SetEnabled(view, false);
+
+            // remove references
+            onTouchAction = null;
+            scalePixels = null;
+        }
+
+        public override void TouchesBegan(NSSet touches, UIEvent evt)
+        {
+            base.TouchesBegan(touches, evt);
+
+            foreach (UITouch touch in touches.Cast<UITouch>())
+            {
+                if (!FireEvent(SKTouchAction.Pressed, touch, true))
                 {
                     IgnoreTouch(touch, evt);
                 }
-			}
-		}
-
-		public override void TouchesMoved(NSSet touches, UIEvent evt)
-		{
-			base.TouchesMoved(touches, evt);
-
-			foreach (UITouch touch in touches.Cast<UITouch>())
-			{
-				FireEvent(SKTouchAction.Moved, touch, true);
-			}
-		}
-
-		public override void TouchesEnded(NSSet touches, UIEvent evt)
-		{
-			base.TouchesEnded(touches, evt);
-
-			foreach (UITouch touch in touches.Cast<UITouch>())
-			{
-				FireEvent(SKTouchAction.Released, touch, false);
-                _touches.Remove(touch);
             }
-		}
+        }
 
-		public override void TouchesCancelled(NSSet touches, UIEvent evt)
-		{
-			base.TouchesCancelled(touches, evt);
-
-			foreach (UITouch touch in touches.Cast<UITouch>())
-			{
-				FireEvent(SKTouchAction.Cancelled, touch, false);
-                _touches.Remove(touch);
-			}
-		}
-
-        public override void Reset()
+        public override void TouchesMoved(NSSet touches, UIEvent evt)
         {
-            base.Reset();
+            base.TouchesMoved(touches, evt);
 
-            foreach (var touch in _touches)
+            foreach (UITouch touch in touches.Cast<UITouch>())
+            {
+                FireEvent(SKTouchAction.Moved, touch, true);
+            }
+        }
+
+        public override void TouchesEnded(NSSet touches, UIEvent evt)
+        {
+            base.TouchesEnded(touches, evt);
+
+            foreach (UITouch touch in touches.Cast<UITouch>())
+            {
+                FireEvent(SKTouchAction.Released, touch, false);
+            }
+        }
+
+        public override void TouchesCancelled(NSSet touches, UIEvent evt)
+        {
+            base.TouchesCancelled(touches, evt);
+
+            foreach (UITouch touch in touches.Cast<UITouch>())
             {
                 FireEvent(SKTouchAction.Cancelled, touch, false);
             }
-
-            _touches.Clear();
         }
 
         private bool FireEvent(SKTouchAction actionType, UITouch touch, bool inContact)
-		{
-			if (onTouchAction == null || scalePixels == null)
-				return false;
+        {
+            if (onTouchAction == null || scalePixels == null)
+                return false;
 
-			var id = touch.Handle.ToInt64();
+            var id = touch.Handle.ToInt64();
 
-			var cgPoint = touch.LocationInView(View);
-			var point = scalePixels(cgPoint.X, cgPoint.Y);
+            var cgPoint = touch.LocationInView(View);
+            var point = scalePixels(cgPoint.X, cgPoint.Y);
 
-			var args = new SKTouchEventArgs(id, actionType, point, inContact);
-			onTouchAction(args);
-			return args.Handled;
-		}
-	}
+            var args = new SKTouchEventArgs(id, actionType, point, inContact);
+            onTouchAction(args);
+            return args.Handled;
+        }
+    }
 
     public abstract class TestSKCanvasViewRendererBase<TFormsView, TNativeView> : ViewRenderer<TFormsView, TNativeView>
         where TFormsView : SKCanvasView
